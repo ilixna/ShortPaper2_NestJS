4f47d47aa3734b3f5642458c9ddb8e1d64cc409d
ilixna <ilianadias9@gmail.com>
2022-06-09 23:06:27 -0400

    Arreglos menores en clases Controller, entity y service

--- src/Genero/genero.entity.ts
+++ src/Genero/genero.entity.ts
@@ -2,7 +2,6 @@ import { DoctorEntity } from 'src/doctor/doctor.entity';
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Genero } from './genero.model';
 
-
 @Entity()
 export class GeneroEntity extends Genero {
   @PrimaryGeneratedColumn()
